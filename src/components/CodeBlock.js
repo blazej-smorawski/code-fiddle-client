27e25c93973c1faaed901e824642cde0f5e9849d
blazej-smorawski <blazej.smorawski@gmail.com>
2022-10-02 18:07:54 +0200

    Python working

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -2,16 +2,22 @@ import React, { useState, useEffect } from 'react';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-python';
 import '../utils/prism-dracula.css';
 
 import { Sheet, Typography, Badge, Button, Box} from '@mui/joy';
 import Card from '@mui/joy/Card';
+import LoadingCard from './LoadingCard';
 
 
 function CodeBlock(props) {
     const [code, setCode] = useState(props.code);
     const result = props.result
+
+    let resultCard = <LoadingCard ratio="16/2"/>;
+    if(result != '') {
+        resultCard = <Card variant='soft' sx={{'white-space': 'pre-wrap'}}> {result} </Card>
+    }
 
     return (
         <Sheet variant='plain' sx={{width:'100%', height:'100%', display: 'flex', flexDirection:'column', gap:'4px', bgcolor:'transparent', width:'auto'}}>
@@ -20,7 +26,7 @@ function CodeBlock(props) {
                 <Editor
                     value={code}
                     onValueChange={code => setCode(code)}
-                    highlight={code => highlight(code, languages.js)}
+                    highlight={code => highlight(code, languages.python)}
                     padding={10}
                     style={{
                         fontFamily: '"Fira code", "Fira Mono", monospace',
@@ -39,9 +45,7 @@ function CodeBlock(props) {
                 </Box>
             </Card>
             <Badge badgeContent='Wynik' color='primary' variant='outlined' anchorOrigin={{vertical: 'top',horizontal: 'left',}}/>
-            <Card variant='soft'>  
-                {result}
-            </Card>
+            {resultCard}
         </Sheet>
     );
 }
--- src/components/CodeSpace.js
+++ src/components/CodeSpace.js
@@ -1,119 +1,41 @@
 import CodeBlock from "./CodeBlock";
-import PixelsBlock from "./PixelsBlock";
-import * as utils from "../utils/arrays"
 
 import React, { useState, useEffect } from 'react';
-import Interpreter from 'js-interpreter';
 import Grid from '@mui/material/Unstable_Grid2';
-import LoadingCard from './LoadingCard';
 
 function CodeSpace(props) {
-    const [pixels, setPixels] = useState(utils.createArray(8,8));
-    const [pixelsLoaded, setPixelsLoaded] = useState(false);
-    const [resultPixels, setResultPixels] = useState(utils.createArray(8,8));
-    const [result, setResult] = useState('')
-
+    const [result, setResult] = useState('Czekam...\ntest\n t\n')
+    const ip = "server.blazej-smorawski.com"
     const exampleCode = `
-// Na ten moment dodałem dwie funkcje do manipulacji pixeli.
-// Tablica pixeli to obiekt o nazwie pixels
-wybierz(4,4);
-ustawKolor(5);
+x = 5
+for i in range(10):
+    print(i)
 
-for(var x=0;x<8;x++) {
-  wybierz(x,x);
-  ustawKolor(7);
-}
 
-for(var x=0;x<8;x++) {
-  pixels[x][0]=0;
-}
+class kot:
+    def __init__(self):
+        self.kolor = "czarny"
+
+burek = kot()
+print(burek.kolor)
 `;
-    const preCode = `
-var pixels = JSON.parse(getPixels());
-// sp - selected pixel - wybrany pixel
-var sp=[0,0];
-
-function wybierz(x,y) {
-    sp[0] = x;
-    sp[1] = y;
-}
-
-function ustawKolor(val) {
-    pixels[sp[1]][sp[0]] = val;
-}
-`;
-    const postCode = `
-JSON.stringify(pixels);
-`;
-
-    useEffect( () => {
-        console.log('Pixele');
-        for(var x = 0; x<pixels.length; x++) {
-            for(var y = 0; y<pixels[x].length; y++) {
-                let minimum = 0;
-                let maximum = 7;
-                pixels[x][y] = (Math.random() * (maximum - minimum + 1) ) << 0;
-                resultPixels[x][y] = 0;
-            }
-        }
-        setPixelsLoaded(true);
-    }, []);
-
-    function initInterpreter(interpreter, globalObject) {
-        var wrapper = function getPixels() {
-            console.log(pixels);
-            return JSON.stringify(pixels);
-        };
-        interpreter.setProperty(globalObject, 'getPixels',
-            interpreter.createNativeFunction(wrapper));
-    }
 
     const runCode = (code) => {
-        console.log('Running: ' + preCode+code+postCode);
-        const interpreter = new Interpreter(preCode+code+postCode, initInterpreter);
-
-        var steps = 0;
-        setResult('Przetwarzam...');
-        try {
-            while(interpreter.step()) {
-                steps++;
-                if(steps>1000) {
-                    setResult('Kod wykonywał się zbyt długo :(');
-                }
-            }
-        } catch (error) {
-            setResult('Błąd: ' + error)
-            return;
-        }
-        console.log('Result: ' + interpreter.value);
-        setResult("Sukces!");
-        setResultPixels(JSON.parse(interpreter.value));
+        setResult('')
+        fetch("http://"+ip+":8080/send_code", {
+            method: "POST",
+            body: JSON.stringify({code: code.split('\n')}),
+            headers: { 'Content-type': "application/json"}
+        }).then(response => response.json()).then(data => {
+            setResult(data.stdout.join('\n'))})
     }
 
-    return pixelsLoaded ?(
+    return (
         <Grid sx={{m: 1 }} container spacing={{ xs: 2, md: 3 }}>
-            <Grid xs={12} sm={3}>
-                <PixelsBlock pixels={pixels}/>
-            </Grid>
-            <Grid xs={12} sm={6}>
+            <Grid xs={12} sm={8}>
                 <CodeBlock code={exampleCode} result={result} runCode={runCode}/>
             </Grid>
-            <Grid xs={12} sm={3}>
-                <PixelsBlock pixels={resultPixels}/>
-            </Grid>
-        </Grid>
-    ):(
-        <Grid sx={{m: 1 }} container spacing={{ xs: 2, md: 3 }}>
-            <Grid xs={12} sm={3}>
-                <LoadingCard ratio='1/1'/>
-            </Grid>
-            <Grid xs={12} sm={6}>
-                <CodeBlock code={exampleCode} result={result} runCode={runCode}/>
-            </Grid>
-            <Grid xs={12} sm={3}>
-                <LoadingCard ratio='1/1'/>
-            </Grid>
-        </Grid>
+        </Grid>  
     );
 }
 
